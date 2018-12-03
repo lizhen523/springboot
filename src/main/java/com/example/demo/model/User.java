5ab0f7745e24502c98ea08c0962329addce0ce68
lizhen523 <980480272@qq.com>
2018-12-03 16:16:09 +0800

    修改实体类

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,5 +1,7 @@
 package com.example.demo.model;
 
+import lombok.Data;
+
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -8,34 +10,11 @@ import javax.persistence.Table;
  * @Author sgl
  * @Date 2018-05-02 14:59
  */
+@Data
 @Table(name = "tbl_user")
 public class User {
     @Id
     private Long userId;
     private String userName;
     private Integer userAge;
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public Integer getUserAge() {
-        return userAge;
-    }
-
-    public void setUserAge(Integer userAge) {
-        this.userAge = userAge;
-    }
 }
