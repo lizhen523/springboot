d0f87284be181367300666df29e16663f62874b6
lizhen523 <980480272@qq.com>
2018-12-03 16:09:50 +0800

    修改user注入

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RestController
 public class HelloController {
-    @Autowired
+    @Resource
     private User user;
     @Autowired
     private TestEntity testEntity;
